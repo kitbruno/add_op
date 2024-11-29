e0c9058e61d2d2e36aca25a3860d261f4896b84e
Kit <kitwana.bruno@gmail.com>
2024-11-29 16:29:21 -0500

    added godoc comments

--- operations/add.go
+++ operations/add.go
@@ -1,5 +1,9 @@
+// Operations provide various utilities to perform on integers (addition, subtraction, division, multiplication, etc.)
 package operations
 
+// Add sums the two integer values provided and returns the new value.
+//
+// It has two parameters which are both integers. Refer to https://www.mathisfun.com/numbers/addition.html for more info.
 func Add(a, b int) int {
 	return a + b
 }
